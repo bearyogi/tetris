244438563f29b43e641d365e30c7cafbab88fe3f
Michał Mamla <mmichal1999@gmail.com>
2021-01-10 18:21:07 +0100

    Added Javadoc to ZBlock.java

--- src/main/java/com/epam/prejap/tetris/block/ZBlock.java
+++ src/main/java/com/epam/prejap/tetris/block/ZBlock.java
@@ -1,5 +1,12 @@
 package com.epam.prejap.tetris.block;
 
+/**
+ * ZBlock is type of block shape existing in Tetris game
+ *
+ * @implNote This class contains static 2d array of byte types to represent "Z-shape" on the board.
+ * @see Block
+ */
+
 final class ZBlock extends Block {
 
     private static final byte[][] IMAGE = {
